1fc84006c40208f3921fc87a0519f47c03ee508d
Raphael A. Alves <raphael.andrade98@outlook.com>
2020-03-21 21:26:37 -0300

    Corrigido menu e banner da landing page e add secao de Sobre

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,32 +36,37 @@ class LoginController extends Controller
         $facebookUserId = $user->id;
         $token = $user->token;
         $user = new User((array) $user);
-        $alreadyRegistered = User::with(['socialMediaUserIds' => function($query) use($user, $facebookUserId) {
-            $query->where('value', $facebookUserId);
-        }])->first();
+        $alreadyRegistered = User::with(['socialMediaUserIds' => 
+            function($query) use($user, $facebookUserId) {
+                $query->where('value', $facebookUserId);
+            }])->first();
         if(!$alreadyRegistered){ // Not registered yet
-            DB::beginTransaction();
-            try{
-                $user->save();
-                $socialMediaIds = new \App\SocialMediaUserId([
-                    'value' => $facebookUserId,
-                    'social_media_id' => SocialMedia::FACEBOOK
-                ]);
-                $user->socialMediaUserIds()->save($socialMediaIds);
-                $token = new Token([
-                    'token' => $token,
-                    'social_media_id' => SocialMedia::FACEBOOK,
-                    'token_type_id' => TokenType::USER
-                ]);
-                $user->tokens()->save($token);
-                DB::commit();
-            } catch(\Exception $e){
-                DB::rollback();
-                throw $e;
-            }
+            $this->singIn($user, $token, $facebookUserId);
         }
         Auth::login($user, true);
         return redirect()->route('dashboard');
+    }
+
+    private function signIn($user, $token, $faceId){
+        DB::beginTransaction();
+        try{
+            $user->save();
+            $socialMediaIds = new \App\SocialMediaUserId([
+                'value' => $faceId,
+                'social_media_id' => SocialMedia::FACEBOOK
+            ]);
+            $user->socialMediaUserIds()->save($socialMediaIds);
+            $token = new Token([
+                'token' => $token,
+                'social_media_id' => SocialMedia::FACEBOOK,
+                'token_type_id' => TokenType::USER
+            ]);
+            $user->tokens()->save($token);
+            DB::commit();
+        } catch(\Exception $e){
+            DB::rollback();
+            throw $e;
+        }
     }
 
     public function logout(){
--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -3,27 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
     <link rel="stylesheet" href="{{asset('semanticui/semantic.min.css')}}">
-    <title>@yield('title')</title>
+    <title>Postr - @yield('title')</title>
 </head>
 <body>
-    <nav id="menu">
-        <di>
-            <span id="logo">Postr</span>
-        </div>
-        <div class="links">
-            @if(!Auth::check())
-                <a href="#about">Sobre</a>
-                <a href="#plans">Planos</a>
-                <a href="#contact">Contato</a>
-                <a href="{{route('login')}}">Entrar</a>
-            @endif
-        </div>
-    </nav>
-
+    
     @yield('body')
-
     <footer>
         {{date("Y")}}
     </footer>
