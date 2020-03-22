ef1d3d7e7302660f636108c566a27119348d24c2
Raphael A. Alves <raphael.andrade98@outlook.com>
2020-03-21 21:51:31 -0300

    Add planos para landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <link rel="stylesheet" href="{{asset('semanticui/semantic.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <title>Postr - home</title>
 </head>
 <body>
@@ -27,7 +27,7 @@
             Automatize sua campanha de marketing em redes socias
         </div>
     </section>
-    <section id="about" class="text-section">
+    <section id="about" class="text-section center">
         <h1>Sobre</h1>    
         <p>
                 Ipsum incididunt tempor excepteur ea proident adipisicing id. Ullamco enim veniam irure nulla enim sint id est proident consectetur. Lorem excepteur qui anim exercitation nulla velit. Magna exercitation ex esse deserunt et consectetur.Non cupidatat magna magna aute irure irure culpa qui aliquip amet sint fugiat sunt. Aute sunt eiusmod nostrud exercitation excepteur qui laborum deserunt eiusmod Lorem officia consequat. Ipsum aliquip Lorem quis non sit voluptate labore culpa occaecat minim nulla ipsum ex qui. Excepteur officia exercitation id irure ut. Incididunt incididunt eu non eu non irure sunt nostrud dolore. Cupidatat enim ut pariatur cillum tempor enim ex.
@@ -41,6 +41,57 @@
             Voluptate ullamco minim non dolor est sit culpa veniam. Deserunt sunt nisi anim commodo proident sit qui nulla culpa esse ad anim id. Id consequat sit officia eu. Pariatur qui nulla laboris culpa reprehenderit anim nulla voluptate aliquip voluptate.
         </p>    
     </section>
+    <section id="#plans" class="text-section">
+        <h1 class="center"> Planos </h1>
+        <div class="ui card">
+            <div class="content">
+                <div class="header">Starter</div>
+            </div>
+            <div class="content">
+                <h4 class="ui sub header">Beneficios</h4>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Ipsum</li>
+                    <li>Dolor</li>
+                </ul>
+            </div>
+            <div class="extra content">
+                <button class="ui green button">Adquirir</button>
+            </div>
+        </div>
+        <div class="ui card">
+            <div class="content">
+                <div class="header">Standard</div>
+            </div>
+            <div class="content">
+                <h4 class="ui sub header">Beneficios</h4>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Ipsum</li>
+                    <li>Dolor</li>
+                </ul>
+            </div>
+            <div class="extra content">
+                <button class="ui green button">Adquirir</button>
+            </div>
+        </div>
+        <div class="ui card">
+            <div class="content">
+                <div class="header">Premium</div>
+            </div>
+            <div class="content">
+                <h4 class="ui sub header">Beneficios</h4>
+                <ul>
+                    <li>Lorem</li>
+                    <li>Ipsum</li>
+                    <li>Dolor</li>
+                </ul>
+            </div>
+            <div class="extra content">
+                <button class="ui green button">Adquirir</button>
+            </div>
+        </div>
+    </section>
 
     <footer>
         {{date("Y")}}
@@ -51,4 +102,3 @@
     <script src="{{asset('js/functions.js')}}"></script>
 </body>
 </html>
-    
