6b7f467944fe6274f3f263964f92c2202ce8c2b1
Raphael Alves <raphael.alves@viavarejo.com.br>
2021-11-11 18:16:51 -0300

    Fixed sidebar positioning

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,36 +10,36 @@
     <title>Postr - @yield('title')</title>
 </head>
 <body>
-    <div class="ui bottom attached segment pushable">
-        <div class="ui visible inverted left vertical sidebar menu">
-          <div class="item title">
-            Postr
-          </div>
-          <a href="{{route('post.list')}}" class="item">
-            <i class="sticky note icon"></i>
-            Publicações
-          </a>
-          <a href="{{route('topic.list')}}" class="item">
-            <i class="newspaper icon"></i>
-            Assuntos
-          </a>
-          <a class="item">
-            <i class="calendar icon"></i>
-            Conograma
-          </a>
-          <a href="{{route('logout')}}" class="item">
-            <i class="sign out alternate icon"></i>
-            Sair
-          </a>
+    
+      <div class="ui left visible inverted vertical menu sidebar">
+        <div class="item title">
+          Postr
         </div>
-        <div class="pusher">
-          <div class="ui basic segment" style="padding-right:20%">
-            <h1 class="ui header" id="main-title">@yield('title')</h1>
-            @include('includes.alerts')
-            @yield('body')
-          </div>
+        <a href="{{route('post.list')}}" class="item">
+          <i class="sticky note icon"></i>
+          Publicações
+        </a>
+        <a href="{{route('topic.list')}}" class="item">
+          <i class="newspaper icon"></i>
+          Assuntos
+        </a>
+        <a class="item">
+          <i class="calendar icon"></i>
+          Conograma
+        </a>
+        <a href="{{route('logout')}}" class="item">
+          <i class="sign out alternate icon"></i>
+          Sair
+        </a>
+      </div>
+      <div class="pusher">
+        <div class="ui basic segment" style="padding-right:20%">
+          <h1 class="ui header" id="main-title">@yield('title')</h1>
+          @include('includes.alerts')
+          @yield('body')
         </div>
-    </div>
+      </div>
+      
     <script src="{{asset('js/jquery.min.js')}}"></script>
     <script src="{{asset('semanticui/semantic.min.js')}}"></script>
     <script src="{{asset('js/functions.js')}}"></script>
