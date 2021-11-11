6a9dd30eced43d80571ce14e70a6ee217795632a
Raphael Alves <raphael.alves@viavarejo.com.br>
2021-11-11 20:49:25 -0300

    Schedule model and migration

---
+++ app/Schedule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\SocialMedia\SocialMedia;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    protected $fillable = ['weekday', 'social_media_id', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function socialMedia()
+    {
+        return $this->belongsTo(SocialMedia::class);
+    }
+
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+        $this->user_id = $user->id;
+        return $this;
+    }
+
+    public function setSocialMedia(SocialMedia $socialMedia)
+    {
+        $this->socialMedia = $socialMedia;
+        $this->social_media_id = $socialMedia->getId();
+        return $this;
+    }
+}
---
+++ database/migrations/2021_11_11_204054_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('weekday');
+            $table->unsignedInteger('social_media_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('social_media_id')->references('id')->on('social_media');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
